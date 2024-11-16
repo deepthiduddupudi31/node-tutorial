7a2f221f727fd502945fdd5029a362fece49328e
duddupudideepthi31 <duddupudideepthi31@gmail.com>
2024-11-16 19:32:52 +0530

    second commit

--- app.js
+++ app.js
@@ -7,7 +7,7 @@
 // const main=require('./module2');
 // console.log(main.add(1,2));
 // console.log(main.sub(3,2));
-const {add,sub}=require('./module2');
-console.log(add(1,2));
-console.log(sub(3,2));
+// const {add,sub}=require('./module2');
+// console.log(add(1,2));
+// console.log(sub(3,2));
 
