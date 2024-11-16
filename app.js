ddde15f253e24cc3c3ea193c643cac24d5fc6abf
duddupudideepthi31 <duddupudideepthi31@gmail.com>
2024-11-16 19:31:08 +0530

    second commit

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-console.log("deepthi");
-console.log("hi");
-console.log("deepthi");
-console.log("hi");
-console.log("deepthi");
-console.log("hi");
-console.log("deepthi");
-console.log("hi");
-console.log("deepthi");
-console.log("hi");
+// const math=require('./module1.js');
+// console.log(math.add(1,2));
+// console.log(math.sub(3,2));
+// const {add,sub}=require('./module1.js');
+// console.log(add(1,2));
+// console.log(sub(3,2));
+// const main=require('./module2');
+// console.log(main.add(1,2));
+// console.log(main.sub(3,2));
+const {add,sub}=require('./module2');
+console.log(add(1,2));
+console.log(sub(3,2));
+
